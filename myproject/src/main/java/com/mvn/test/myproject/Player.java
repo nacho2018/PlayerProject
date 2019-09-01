2f5725b507c555ef8e2b34794e73192d0a48a442
acme <acme@acme-pc>
2019-09-01 12:21:26 +0200

    Initial commit of project.

---
+++ myproject/src/main/java/com/mvn/test/myproject/Player.java
@@ -0,0 +1,72 @@
+package com.mvn.test.myproject;
+
+/**
+ * The Class Player.
+ */
+public class Player {
+	
+	/** The name. */
+	private String name;
+	
+	/** The game. */
+	private String game;
+	
+	/** The age. */
+	private int age;
+	
+	/**
+	 * Gets the name.
+	 *
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	
+	/**
+	 * Sets the name.
+	 *
+	 * @param name the new name
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	/**
+	 * Gets the game.
+	 *
+	 * @return the game
+	 */
+	public String getGame() {
+		return game;
+	}
+	
+	/**
+	 * Sets the game.
+	 *
+	 * @param game the new game
+	 */
+	public void setGame(String game) {
+		this.game = game;
+	}
+	
+	/**
+	 * Gets the age.
+	 *
+	 * @return the age
+	 */
+	public int getAge() {
+		return age;
+	}
+	
+	/**
+	 * Sets the age.
+	 *
+	 * @param age the new age
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+
+}
---
+++ myproject/src/main/java/com/mvn/test/myproject/TestPlayer.java
@@ -0,0 +1,21 @@
+package com.mvn.test.myproject;
+
+public class TestPlayer {
+
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
+	public static void main(String[] args) {
+		
+		Player player1 = new Player();
+		player1.setName("Andrew");
+		player1.setAge(12);
+		player1.setGame("Game of Thrones");
+		
+		System.out.println("Info on player1: " + player1.getName() + ", " + player1.getGame() + ", " + player1.getAge());
+
+	}
+
+}
